55f845bb83a47186da96cdfda248ff26bf50ccae
Valeryia Klopava <bsu.valeria.kl@gmail.com>
2020-10-24 19:34:48 +0300

    init: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+    if (n < 0) {
+        n = n * -1;
+        let str = String(n).split('').reverse().join('');
+    return (+str);
+    }
+    else {
+        let str = String(n).split('').reverse().join('');
+    return (+str);
+    }
+};
